e373fe8af0effc2f31d76c9e0c95e41948e7d208
ArnautVasseur <vasseur.arnaut@gmail.com>
2022-10-25 15:10:46 +0200

    Create button responsive account

--- src/components/buttons/Compte.vue
+++ src/components/buttons/Compte.vue
@@ -1,3 +1,6 @@
 <template>
-    <div class="w-BoutonCompte bg-black text-white">Compte</div>
+    <button class="absolute top-[30px] flex justify-center gap-5 items-center text-center right-[30px] lg:w-BoutonCompte w-40 h-9 lg:h-16 rounded-md bg-DarkerGray">
+        <span class="text-white lg:text-xl">Compte</span>
+        <i class="fa-solid fa-user text-white lg:text-xl"></i>
+    </button>
 </template>
