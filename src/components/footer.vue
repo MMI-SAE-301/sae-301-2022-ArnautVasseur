8866a9b91f7c96e3e87695bc66d15cae07e7f71d
ArnautVasseur <vasseur.arnaut@gmail.com>
2022-10-25 16:40:37 +0200

    Adding LogoGoogle and LogoFacebook in asset and footer

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,13 +1,17 @@
 <template>
-    <div class="flex absolute bg-DarkBlue opacity-30 w-full h-64 bottom-0"></div>
-    <div class="absolute bottom-20">
-        <RouterLink to="/liste">
-            <div
-                class="rounded-3xl bg-DarkerGray lg:w-96 lg:h-24 hidden lg:flex text-white text-2xl items-center justify-center">
-                Liste de montres
-            </div>
-        </RouterLink>
-
-        
+    <div class="flex absolute justify-around items-center bg-DarkBlue bg-opacity-30 w-full h-64 bottom-0">
+        <div class="">
+            <RouterLink to="/liste">
+                <div
+                    class="rounded-3xl bg-DarkerGray opacity-100 lg:w-96 lg:h-24 hidden lg:flex text-white text-2xl items-center justify-center">
+                    Liste de montres
+                </div>
+            </RouterLink>
+        </div>
+        <h3 class="text-white">Connexion</h3>
+        <div class="w-[424px] h-44 flex items-center justify-evenly bg-DarkerGray rounded-3xl">
+            <img src="../assets/Google.png" alt="LogoGoogle">
+            <img src="../assets/Facebook.png" alt="LogoFacebook">
+        </div>
     </div>
 </template>
