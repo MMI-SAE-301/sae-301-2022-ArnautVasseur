453eba4bb05e5928f7f389741f35cd420d6e4aa0
ArnautVasseur <vasseur.arnaut@gmail.com>
2022-10-26 08:16:35 +0200

    Finishing responsive footer

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,17 +1,25 @@
 <template>
-    <div class="flex absolute justify-around items-center bg-DarkBlue bg-opacity-30 w-full h-64 bottom-0">
-        <div class="">
-            <RouterLink to="/liste">
-                <div
-                    class="rounded-3xl bg-DarkerGray opacity-100 lg:w-96 lg:h-24 hidden lg:flex text-white text-2xl items-center justify-center">
-                    Liste de montres
-                </div>
-            </RouterLink>
+  <div class="flex absolute justify-around items-center bg-DarkBlue bg-opacity-30 w-full h-32 lg:h-64 bottom-0">
+    <div>
+      <RouterLink to="/liste">
+        <div
+          class="rounded-3xl bg-DarkerGray opacity-100 w-44 h-14 text-lg lg:w-96 lg:h-24 flex text-white lg:text-2xl items-center justify-center">
+          Liste de montres
         </div>
-        <h3 class="text-white">Connexion</h3>
-        <div class="w-[424px] h-44 flex items-center justify-evenly bg-DarkerGray rounded-3xl">
-            <img src="../assets/Google.png" alt="LogoGoogle">
-            <img src="../assets/Facebook.png" alt="LogoFacebook">
-        </div>
+      </RouterLink>
     </div>
+    <div class="w-[424px] h-44 items-center justify-evenly hidden lg:flex bg-DarkerGray rounded-3xl">
+      <img src="../assets/Google.png" alt="LogoGoogle">
+      <img src="../assets/Facebook.png" alt="LogoFacebook">
+    </div>
+    <!-- Début Bouton Account -->
+    <RouterLink to="/account">
+      <div
+        class="flex justify-center gap-5 items-center text-center right-[30px] lg:w-BoutonCompte w-40 h-9 lg:h-16 rounded-md dark:bg-DarkerGray bg-DarkGray">
+        <span class="text-white lg:text-xl">Compte</span>
+        <i class="fa-solid fa-user text-white lg:text-xl"></i>
+      </div>
+    </RouterLink>
+  <!-- Fin Bouton Account -->
+  </div>
 </template>
--- src/components/header.vue
+++ src/components/header.vue
@@ -1,20 +1,20 @@
 <template>
-    <header>
-        <div class="flex absolute justify-center items-center gap-5 top-3 -ml-24">
-            <img src="../assets/Logo.svg" alt="Logo" class="w-20 lg:w-40 hidden sm:flex">
-            <h1 class="dark:text-white text-DarkerGray text-2xl font-semibold hidden xl:flex">TiK•TaK</h1>
-        </div>
+  <header>
+    <div class="flex absolute justify-center items-center gap-5 top-3 -ml-24">
+      <img src="../assets/Logo.svg" alt="Logo" class="w-20 lg:w-40 hidden sm:flex">
+      <h1 class="dark:text-white text-DarkerGray text-2xl font-semibold hidden xl:flex">TiK•TaK</h1>
+    </div>
 
-        <!-- Début Bouton Account -->
-        <RouterLink to="/account">
-            <div
-                class="absolute top-[30px] flex justify-center gap-5 items-center text-center right-[30px] lg:w-BoutonCompte w-40 h-9 lg:h-16 rounded-md dark:bg-DarkerGray bg-DarkGray">
-                <span class="text-white lg:text-xl">Compte</span>
-                <i class="fa-solid fa-user text-white lg:text-xl"></i>
-            </div>
-        </RouterLink>
-    </header>
-    <!-- Fin Bouton Account -->
+    <!-- Début Bouton Account -->
+    <RouterLink to="/account">
+      <div
+        class="absolute top-[30px] flex justify-center gap-5 items-center text-center right-[30px] lg:w-BoutonCompte w-40 h-9 lg:h-16 rounded-md dark:bg-DarkerGray bg-DarkGray">
+        <span class="text-white lg:text-xl">Compte</span>
+        <i class="fa-solid fa-user text-white lg:text-xl"></i>
+      </div>
+    </RouterLink>
+  </header>
+  <!-- Fin Bouton Account -->
 </template>
 
 <script lang="ts">
