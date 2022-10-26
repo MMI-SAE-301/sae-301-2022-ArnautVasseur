41027f3492602aab58036f2922f556a81278d62b
ArnautVasseur <vasseur.arnaut@gmail.com>
2022-10-26 20:05:13 +0200

    Adding dark mode style to account button

--- src/components/header.vue
+++ src/components/header.vue
@@ -7,9 +7,9 @@
       </RouterLink>
 
       <!-- Début Bouton Account -->
-      <RouterLink to="/account" class="absolute top-[30px] flex justify-center gap-5 items-center text-center right-[30px] lg:w-BoutonCompte w-40 h-9 lg:h-16 rounded-md dark:bg-DarkerGray bg-DarkGray">
-          <span class="text-white lg:text-xl">Compte</span>
-          <i class="fa-solid fa-user text-white lg:text-xl"></i>
+      <RouterLink to="/account" class="absolute top-[30px] flex justify-center gap-5 items-center text-center right-[30px] lg:w-BoutonCompte w-40 h-9 lg:h-16 rounded-md dark:bg-DarkerGray bg-LightGray dark:hover:bg-LightGray">
+          <span class="dark:text-white dark:hover:text-black lg:text-xl">Compte</span>
+          <i class="fa-solid fa-user dark:text-white dark:hover:text-black lg:text-xl"></i>
       </RouterLink>
     </div>
   </header>
