f2c18d6ed3d8620f26faee34f3158ff5944f3e41
ArnautVasseur <vasseur.arnaut@gmail.com>
2022-10-27 14:15:48 +0200

    Adding Ts-Ignore

--- src/components/FormKitListColors.vue
+++ src/components/FormKitListColors.vue
@@ -7,16 +7,15 @@ defineProps<{
 </script>
 <template>
     <FormKit :name="name" :label="label" value="#FFFFFF" type="radio" :options="colors" :sections-schema="{
-        inner: {$el:null},
-        decorator: {$el:null}
-    }" input-class="peer sr-only" options-class="flex gap-5 flex-wrap mb-16 mt-5"
-    :classes="{
-        legend:'text-white text-2xl'
-    }">
+        inner: { $el: null },
+        decorator: { $el: null }
+    }" input-class="peer sr-only" options-class="flex gap-5 flex-wrap mb-16 mt-5" :classes="{
+    legend: 'text-white text-2xl'
+}">
         <template #label="context">
             <div class="h-6 w-6 rounded-[7px] border-2 border-transparent peer-checked:border-white"
-                :style="{backgroundColor:context.option.value}" />
-            <span class="sr-only">{{context.option.label}}</span>
+                :style="{ backgroundColor: context.option.value }" />
+            <span class="sr-only">{{ context.option.label }}</span>
         </template>
     </FormKit>
 </template>
